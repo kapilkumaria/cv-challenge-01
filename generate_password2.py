3c92bc91d8c35004a2300697f3aa093a9164b312
kapilpil kumaria <42500918+kapilkumaria@users.noreply.github.com>
2024-11-26 17:07:04 -0700

    Troubleshooting

--- generate_password2.py
+++ generate_password2.py
@@ -1,5 +1,5 @@
 import bcrypt
 
-new_password = "yyc1512"  # Replace with your desired password
+new_password = "mata1512"  # Replace with your desired password
 hashed_password = bcrypt.hashpw(new_password.encode(), bcrypt.gensalt())
 print(hashed_password.decode())
