e3f3e4ff848057e4072ccaf6046955a46e6f91a0
kapilpil kumaria <42500918+kapilkumaria@users.noreply.github.com>
2024-11-28 08:39:20 -0700

    Stop tracking generate_password.py

--- generate_password2.py
+++
@@ -1,5 +0,0 @@
-import bcrypt
-
-new_password = "Jumio1512"  # Replace with your desired password
-hashed_password = bcrypt.hashpw(new_password.encode(), bcrypt.gensalt())
-print(hashed_password.decode())
